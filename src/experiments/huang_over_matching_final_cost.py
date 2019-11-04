2d6c874debe5004575255c5f34b0cda6858ba521
Henry Wilde <henrydavidwilde@gmail.com>
2019-11-04 20:02:46 +0000

    Write EDO data generation code. (#36)
    * Add env file and source code.
    
    * Add `tqdm` and updated version of `edo_exp` to env
    
    * Move to generalised EDO experiment structure.

---
+++ src/experiments/huang_over_matching_final_cost.py
@@ -0,0 +1,25 @@
+""" Fitness function to produce datasets where Huang's method outperforms the
+matching initialisation in terms of final cost. """
+
+from kmodes.kmodes import KModes
+
+
+def fitness(dataframe, n_clusters=3, seed=0):
+    """ Cluster the data into `n_clusters` parts with each initialisation
+    method. Return the difference between their initial costs so as to minimise
+    that of Huang's method. """
+
+    km_matching = KModes(
+        n_clusters=n_clusters,
+        init="matching",
+        n_init=25,
+        random_state=seed,
+    ).fit(dataframe)
+    km_huang = KModes(
+        n_clusters=n_clusters,
+        init="huang",
+        n_init=25,
+        random_state=seed,
+    ).fit(dataframe)
+
+    return km_huang.cost_ - km_matching.cost_  # EDO minimises by default
